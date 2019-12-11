5d3c118b6a42f6435ca9348ab102e7a15e580517
thangdo0212 <thangdo021297@gmail.com>
2019-12-11 23:45:10 +0700

    fix: error nuil cms

--- src/app/layouts/admin/admin.component.ts
+++ src/app/layouts/admin/admin.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 @Component({
   selector: 'app-secure',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css', './css/adminlte.css'],
+  styleUrls: ['./admin.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class AdminComponent implements OnInit {
